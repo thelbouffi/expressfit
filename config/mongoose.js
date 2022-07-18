c704b03a3d0e65d1dd5aa5a8cd56f344f7671a72
Taha <taha@Tahas-MacBook-Pro-2.local>
2022-07-18 15:25:13 +0000

    remove shalk 04

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,22 +1,20 @@
 const mongoose = require("mongoose");
-// import { red, yellow } from 'chalk';
-const { red, yellow } = require('chalk');
 
 // Initialize Mongoose
 exports.connect = async (db) => {
   try {
     await mongoose.connect(db);
   } catch (error) {
-    console.error(red('Could not connect to MongoDB!'));
-    // console.error('Could not connect to MongoDB!');
+    // console.error(red('Could not connect to MongoDB!'));
+    console.error('Could not connect to MongoDB!');
     console.log(error);
   }
 };
 
 exports.disconnect = (cb) => {
   mongoose.connection.close((err) => {
-    console.info(yellow("Disconnected from MongoDB."));
-    // console.info("Disconnected from MongoDB.");
+    // console.info(yellow("Disconnected from MongoDB."));
+    console.info("Disconnected from MongoDB.");
     return cb(err);
   });
 };
